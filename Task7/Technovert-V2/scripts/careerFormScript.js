bdcbf6e1d810d581b4f26977894604d04e6480dc
L N S S Ravi Teja <raviteja6820@gmail.com>
2021-03-05 14:34:24 +0530

    careerFormUpdate

--- Task7/Technovert-V2/scripts/careerFormScript.js
+++ Task7/Technovert-V2/scripts/careerFormScript.js
@@ -14,8 +14,10 @@ function validateCareerForm(){
 	var desiredRole = document.forms["careerForm"]["your-role"].value;
     var resume = document.forms["careerForm"]["your-resume"].value;
 
-    console.log(desiredRole);
-	  
+    if(!validateEmail(email) && email.length >= 1){
+        document.getElementById('emailHelp').innerHTML = "Enter in correct pattern [eg., xyz.123_abc@hyd.co.in]";
+    }
+
     window.scrollTo({top: 0, behavior: 'smooth'});      
     if(!validateEmail(email) || name.length<1 || email.length<1 || desiredRole.length === '' || resume.length < 1){
         document.getElementById("warning").innerHTML = "<h6 class='fw-bold text-danger m-2 ms-3'>Please fill all the fields below</h6>";
